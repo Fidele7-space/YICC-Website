b31bbcfbb3fe7219d56ae5639f0146e56dc6c389
Your Name <fidelenyandwi77@gmail.com>
2026-01-19 22:18:20 +0800

    Look at the current version

--- yicc-website/Files' Folders/JS/home.js
+++ yicc-website/Files' Folders/JS/home.js
@@ -16,3 +16,51 @@
     lastScrollY = currentScrollY;
   });
 
+
+  /* Storing Username and message information */
+ document.addEventListener("DOMContentLoaded", ()=> {
+  const form =document.getElementById('contactForm');
+
+  form.addEventListener("submit", async (e) =>{
+    e.preventDefault(); //preventing page reload
+
+    try{
+      const submitBtn = document.getElementById('#submitBtn').value.trim();
+      const username = document.getElementById('#username').value.trim();
+      const email = document.getElementById('#email').value.trim();
+      const message = document.getElementById('#message');
+
+      if (username.length < 6){
+        throw new Error("Name Full name must be at least 6 characters");
+      }
+      
+      if(!email.contains("@")){
+        throw new Error("Your email should contain @ symbol");
+      }
+
+      if(message.length < 10){
+        throw new Error("The message should be at least 10 characters");
+      }
+      
+    //-----------SEND TO BACKEND-------------
+
+      const response = await fetch("/api/contact", {
+      method: "POST", 
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({username, email, message})
+      });
+
+      if(!response.ok){
+        throw new Error("Something went wrong. Please try again later.");
+      }
+
+      form.reset();
+    } catch(error){
+      alert(error.message);
+    }
+  });
+ });
+
+
+
+
